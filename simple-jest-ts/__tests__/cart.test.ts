0d925d97e48ac21324fb17856737c26da2a149f0
Michael Timbs <mtimbs91@gmail.com>
2019-08-28 00:50:29 +0930

    Show that no assertions result in passing test

--- simple-jest-ts/__tests__/cart.test.ts
+++ simple-jest-ts/__tests__/cart.test.ts
@@ -53,3 +53,33 @@ test('adding an item that is already in the cart does not increase the number of
   // Assert
   expect(cart).toHaveLength(1);
 });
+
+
+test('No assertions will result in a passing test', () => {
+// Setup
+  const item: Item = {
+    name: 'Some really good item',
+    sku: 'SKU_FOO_BAR_BAZ_123',
+    price: 2999,
+  };
+  const cart: Cart = [cartItem(item, 1)];
+
+  // Act
+  addItemToCart(item, cart);
+
+  // Assert
+  // TODO: Write some assertion for this case
+});
+
+test('Async code will hit end of test before resolving and mark as pass because no assertion', () => {
+  setTimeout(() => {
+    expect(true).toBe(false)
+  }, 100)
+});
+
+
+test('Async code will hit end of test before resolving and mark as pass because no assertion', () => {
+  setTimeout(() => {
+    expect(true).toBe(false)
+  }, 100)
+});
